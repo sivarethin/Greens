6f7cce66850f099dea16f5a03ae73f4c68494e6a
sivarethin <sivarethin@gmail.com>
2022-08-06 10:16:01 +0530

    greens

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,44 @@
+package org.add;
+
+public class GreensTech {
+	private void greensomr() {
+
+		System.out.println("Greens OMR");
+	}
+	private void greensAdayar() {
+		
+		System.out.println("Greens Adayar");
+
+	}
+
+	private void greensThambaram() {
+		
+		System.out.println("Greens Thambaram");
+
+	}
+	
+	private void greensVelacheri() {
+		
+		System.out.println("Greens Velacheri");
+
+	}
+
+	private void greensAnnanager() {
+		
+		System.out.println("Greens Annanager");
+
+	}
+	public static void main(String[] args) {
+		
+		GreensTech gt = new GreensTech();
+		
+		gt.greensomr();
+		gt.greensAdayar();
+		gt.greensThambaram();
+		gt.greensVelacheri();
+		gt.greensAnnanager();
+		
+		
+		
+	}
+}
